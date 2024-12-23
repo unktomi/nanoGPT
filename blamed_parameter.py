48719383e97e092615fb943f3351dd52c3e633c6
unktomi <christopher.d.oliver@gmail.com>
2024-12-22 17:57:03 -0800

    Fix BlamedParameter to wrap nn.Parameter instead of extending it

--- blamed_parameter.py
+++ blamed_parameter.py
@@ -1,26 +1,31 @@
 import torch
 import torch.nn as nn
 
-class BlamedParameter(torch.nn.Parameter):
-    def __new__(cls, data, requires_grad=True):
-        # Create the Parameter properly
-        instance = super().__new__(cls, data, requires_grad=requires_grad)
+class BlamedParameter:
+    """Wrapper around nn.Parameter that adds blame tracking"""
+    
+    def __init__(self, parameter):
+        self.param = parameter if isinstance(parameter, nn.Parameter) else nn.Parameter(parameter)
+        self.activation = torch.zeros_like(self.param.data)
+        self.blame = torch.zeros_like(self.param.data)
+        self.decay = 0.99
         
-        # Add our tracking tensors
-        instance.activation = torch.zeros_like(data)
-        instance.blame = torch.zeros_like(data)
-        instance.decay = 0.99
-        
-        return instance
-
     def update(self, current_activation):
         # Update activation (reward)
         self.activation = self.decay * self.activation + (1 - self.decay) * current_activation.abs()
         
         # Update blame (penalty) if we have gradients
-        if self.grad is not None:
-            self.blame = self.decay * self.blame + (1 - self.decay) * self.grad.abs()
+        if self.param.grad is not None:
+            self.blame = self.decay * self.blame + (1 - self.decay) * self.param.grad.abs()
 
     def get_update_weight(self):
         # Simple reward/penalty ratio 
         return self.blame / (self.activation + 1e-8)
+        
+    @property
+    def data(self):
+        return self.param.data
+        
+    @property
+    def grad(self):
+        return self.param.grad
