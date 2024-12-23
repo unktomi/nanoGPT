1f1e65bf9a9dc3fb3ccfe9a2193c6b44618e9f95
unktomi <christopher.d.oliver@gmail.com>
2024-12-22 17:57:18 -0800

    Update optimizer to handle wrapped parameters

--- blame_optimizer.py
+++ blame_optimizer.py
@@ -3,17 +3,20 @@ from torch.optim import Optimizer
 
 class BlameOptimizer(Optimizer):
     def __init__(self, params, lr=1e-3):
-        super().__init__([p for p in params if p.requires_grad], {'lr': lr})
+        # Extract actual parameters from blame wrappers
+        blame_params = [(p.param if hasattr(p, 'param') else p) for p in params]
+        super().__init__(blame_params, {'lr': lr})
+        self.blame_wrappers = [p for p in params if hasattr(p, 'param')]
 
     @torch.no_grad()
     def step(self):
         for group in self.param_groups:
-            for p in group['params']:
+            for p, blame_wrapper in zip(group['params'], self.blame_wrappers):
                 if p.grad is None:
                     continue
                     
                 # Get reward/penalty weighting
-                update_weight = p.get_update_weight()
+                update_weight = blame_wrapper.get_update_weight()
                 
                 # Apply weighted update
                 p.data.add_(p.grad, alpha=-group['lr'] * update_weight)
